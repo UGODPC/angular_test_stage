5e22ff10c4361bb229caff7e31d4527cc6a0b09b
UGOAT <ugodpc2@gmail.com>
2026-07-06 13:54:12 +0200

    Création d'un nouveau composant qui permet de créer/ajouter un livre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ListeLivres } from './liste-livres/liste-livres';
 import { Router } from 'express';
+import { CreateBook } from './create-book/create-book';
 
 export const routes: Routes = [
     {path: 'livres', component: ListeLivres}, //livres pour l'URL et component qui sera affiché
+    {path: 'creerLivre', component: CreateBook},
     {path: '', redirectTo: 'livres', pathMatch: 'full'} //Si le lien c'est juste localhost:4200, on renvoie vers /livres et le chemin match à 100%
 ];
---
+++ src/app/create-book/create-book.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-book',
+  imports: [],
+  templateUrl: './create-book.html',
+  styleUrl: './create-book.css',
+})
+export class CreateBook {}
