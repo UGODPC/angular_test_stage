df4f6c84a26bc9ef3efcc04c7781fec020844441
UGOAT <ugodpc2@gmail.com>
2026-07-06 10:28:44 +0200

    Début

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListeLivres } from './liste-livres/liste-livres';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ListeLivres],
+  imports: [RouterOutlet, ListeLivres, HttpClient],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
