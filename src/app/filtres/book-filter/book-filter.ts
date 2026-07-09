4301365f00f85aa6b7c120e18c42846b52fce6c9
UGOAT <ugodpc2@gmail.com>
2026-07-09 10:03:59 +0200

    Suppression de ce composant parce que pour l'instant, les filtres sont pas gros. C'est juste sur une catégorie !

--- src/app/filtres/book-filter/book-filter.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-book-filter',
-  imports: [],
-  templateUrl: './book-filter.html',
-  styleUrl: './book-filter.css',
-})
-export class BookFilter {}
