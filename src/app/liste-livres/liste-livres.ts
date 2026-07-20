f662a665817932299295ee44a8c463ceb67388ba
UGODPC <ugodpc2@gmail.com>
2026-07-20 14:49:53 +0200

    ..

--- src/app/liste-livres/liste-livres.ts
+++ src/app/liste-livres/liste-livres.ts
@@ -192,6 +192,36 @@ export class ListeLivres implements OnInit, AfterViewInit {
     }
   }
 
+  sortedData = signal(this.desserts.slice());
+
+  sortData(sort: Sort) {
+    const data = this.desserts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData.set(data);
+      return;
+    }
+
+    this.sortedData.set(
+      data.sort((a, b) => {
+        const isAsc = sort.direction === 'asc';
+        switch (sort.active) {
+          case 'name':
+            return compare(a.name, b.name, isAsc);
+          case 'calories':
+            return compare(a.calories, b.calories, isAsc);
+          case 'fat':
+            return compare(a.fat, b.fat, isAsc);
+          case 'carbs':
+            return compare(a.carbs, b.carbs, isAsc);
+          case 'protein':
+            return compare(a.protein, b.protein, isAsc);
+          default:
+            return 0;
+        }
+      }),
+    );
+  }
+
   exportToExcel()
   {
     //1. Construire le contenu HTML du fichier Excel
@@ -290,3 +320,7 @@ export class ListeLivres implements OnInit, AfterViewInit {
     return text.replace(/[&<>"']/g, (m) => map[m]);
   }
 }
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
